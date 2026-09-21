7526763cb919b7f497c8ac06768b40cb13e3bb3e
RichHaker <91494577+RichHaker@users.noreply.github.com>
2026-09-21 08:57:49 +0300

    Плагины Pytest

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -3,7 +3,7 @@ import pytest
 
 @pytest.mark.regression
 @pytest.mark.authorization
-def test_rong_email_or_password_authorization(chromium_page: Page):
+def test_wrong_email_or_password_authorization(chromium_page: Page):
         chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
         email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
