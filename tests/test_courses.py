18ef753996d78b9519e559ff037bad66835dd8c2
RichHaker <91494577+RichHaker@users.noreply.github.com>
2026-09-24 09:34:09 +0300

    Параметризация Pytest

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -5,22 +5,22 @@ import pytest
 @pytest.mark.courses
 @pytest.mark.regression
 def test_empty_courses_list(chromium_page_with_state: Page):
-        chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-        courses = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
-        expect(courses).to_be_visible()
-        expect(courses).to_have_text('Courses')
+    courses = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses).to_be_visible()
+    expect(courses).to_have_text('Courses')
 
-        icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
-        expect(icon).to_be_visible()
+    icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
+    expect(icon).to_be_visible()
 
-        title = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
-        expect(title).to_be_visible()
-        expect(title).to_have_text('There is no results')
+    title = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
+    expect(title).to_be_visible()
+    expect(title).to_have_text('There is no results')
 
-        description = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
-        expect(description).to_be_visible()
-        expect(description).to_have_text('Results from the load test pipeline will be displayed here')
+    description = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
+    expect(description).to_be_visible()
+    expect(description).to_have_text('Results from the load test pipeline will be displayed here')
 
 
 
