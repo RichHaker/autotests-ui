c11e1eaa1267c5e6322ba38250b72e535a3bcc9f
RichHaker <91494577+RichHaker@users.noreply.github.com>
2026-09-26 13:01:33 +0300

    Знакомство с PageObject

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -14,6 +14,7 @@ from pages.login_page import LoginPage
         ("  ", "password")
     ]
 )
+
 def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str, password: str):
     login_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
     login_page.fill_login_form(email=email, password=password)
